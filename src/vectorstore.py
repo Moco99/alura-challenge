df1cf505d1a761aec6ef523437635d7133331084
Moco <abaezsolano@gmail.com>
2026-07-21 15:45:59 -0600

    minor bug fixes

--- src/vectorstore.py
+++ src/vectorstore.py
@@ -1,7 +1,6 @@
 from helper import GEMINI_API_KEY, GEMINI_EMBBEDINGS_MODEL, GEMINI_MODEL
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
-from ingest import chunks
 from ingest import DocsIngestor
 
 class VectorStore():
@@ -10,9 +9,9 @@ class VectorStore():
         self.vectorstore = None
         self.embeddings_model = GoogleGenerativeAIEmbeddings(model=GEMINI_EMBBEDINGS_MODEL, google_api_key=GEMINI_API_KEY)
 
-        def retrieve(self):
-            vectorstore = FAISS.from_documents(self.chunks, self.embeddings_model)
-            retriever = vectorstore.as_retriever(search_type="similarity_score_threshold", 
+    def retrieve(self):
+        vectorstore = FAISS.from_documents(self.chunks, self.embeddings_model)
+        retriever = vectorstore.as_retriever(search_type="similarity_score_threshold", 
                                                  search_kwargs={"score_threshold": 0.3,"k":4} 
                                                 )
-            return retriever
+        return retriever
